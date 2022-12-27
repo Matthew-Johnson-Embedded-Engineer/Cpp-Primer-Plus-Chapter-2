0b26973e103da3529d1c2e400e8284b9c45c3855
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-26 19:54:37 -0600

    Updating myfirst.cpp file

---
+++ Listing-2-1/myfirst.cpp
@@ -0,0 +1,14 @@
+// myfirst.cpp -- displays a message
+
+#include <iostream>					// A PREPROCESSOR directive
+using namespace std;					// make definition visible
+
+int main(void) {					// function header and start of function body
+
+	cout << "Come up and C++ me some time.";	// message
+	cout << endl;					// start a new line
+	cout << "You won't regreet it!" << endl;	// more output
+
+	return 0;					// terminate main()
+}							// end of function bod:w
+							// 
